628524b5f41d5711986402fefa3042e77436d4bd
aouirachid <aouirachid226@gmail.com>
2025-07-08 15:30:53 +0100

    create the 404,422,403 error exaption with missing message

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -26,5 +26,35 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+
+        //
+        $this->reportable(function (ModelNotFoundException $e, $request) {
+            return response()->json([
+                'status' => 404,
+                'message' => 'The resource you request does not exist or has been moved', 
+            ], Response::HTTP_NOT_FOUND);
+        });
+
+        // $this->reportable(function (AuthenticationException $e, $request) {
+        //     return response()->json([
+        //         'status' => 401,
+        //         'message' => '',
+        //     ], Response:: );
+        // });
+
+        $this->reportable(function (AuthorizationException $e, $request) {
+            return response()->json([
+                'status' => 403,
+                'message' => '',
+            ], Response::HTTP_UNAUTHORIZED);
+        });
+
+        $this->reportable(function (ValidationException $e, $request) {
+            return response()->json([
+                'status' => 422,
+                'message' => '',
+                'errors' => $e->errors(),
+            ], Response::HTTP_UPROCESSABLE_ENTITY);
+        });
     }
 }
