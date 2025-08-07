f66b8a0cf4ef8fb5c5a0140b3faba4965709686d
rachidaoui <aouirachid22@gmail.com>
2025-08-07 18:48:24 +0100

    feat(user): Add feature tests for UserController@update
    - Created a new feature test suite for the `UserController@update` endpoint.
    - Tests cover successful profile updates, ensuring database changes are reflected.
    - Includes tests for validation failures, such as invalid email formats and duplicate usernames.
    - Added critical security tests for restricted fields and unauthorized update attempts.

---
+++ tests/Feature/Controller/User/UpdateUserTest.php
@@ -0,0 +1,103 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Tymon\JWTAuth\Facades\JWTAuth;
+use Illuminate\Testing\Fluent\AssertableJson;
+
+
+test("authenticated user can successfully update several of their own profile fields", function () {
+    // ARRANGE
+    $user = User::factory()->create();
+    $token = JWTAuth::fromUser($user);
+
+    $newFirstName = 'Aoui';
+    $newLastName = 'Rachid';
+    $newCity = 'Casablanca';
+
+    $response = $this->putJson(
+        '/api/v1/users/' . $user->id,
+        [
+            'firstName' => $newFirstName,
+            'lastName' => $newLastName,
+            'city' => $newCity,
+        ],
+        [
+            'Authorization' => 'Bearer ' . $token
+        ]
+    );
+
+    // ASSERT
+    $response->assertStatus(200);
+    $response->assertJson([
+        'message' => 'User updated successfully'
+    ]);
+
+    $this->assertDatabaseHas('users', [
+        'id' => $user->id,
+        'firstName' => $newFirstName,
+        'lastName' => $newLastName,
+        'city' => $newCity,
+    ]);
+});
+
+
+it("Fail to update user profile with invalid data", function () {
+    $userToUpdate = User::factory()->create();
+    $otherUser = User::factory()->create(['userName' => 'otherUser', 'phone' => '0766051861']);
+
+    $token = JWTAuth::fromUser($userToUpdate);
+
+    $originalUserName = $userToUpdate->userName;
+    $originalEmail = $userToUpdate->email;
+
+    $response = $this->putJson(
+        '/api/v1/users/' . $userToUpdate->id,
+        [
+            'userName' => 'otherUser',
+            'email' => 'invalidEmail',
+        ],
+        [
+            'Authorization' => 'Bearer ' . $token
+        ]
+    );
+
+    $response->assertStatus(422);
+    $response->assertJsonValidationErrors([
+        'userName' => 'The user name has already been taken.',
+        'email' => 'The email field must be a valid email address.',
+    ]);
+
+    $userToUpdate->refresh();
+    expect($userToUpdate->userName)->toBe($originalUserName);
+    expect($userToUpdate->email)->toBe($originalEmail);
+});
+
+test(" user cannot update a restricted field", function () {
+    $user = User::factory()->create();
+    $token = JWTAuth::fromUser($user);
+    $newData = [
+        'id' => '2',
+        'firstName' => 'Aoui',
+        'lastName' => 'Rachid',
+        'city' => 'Casablanca',
+    ];
+    $originalId = $user->id;
+
+    $response = $this->putJson(
+        '/api/v1/users/' . $user->id,
+        $newData,
+        [
+            'Authorization' => 'Bearer ' . $token
+        ]
+    );
+    $user->refresh();
+    $response->assertStatus(200);
+    $response->assertJson([
+        'message' => 'User updated successfully'
+    ]);
+    expect($user->id)->toBe($originalId);
+    expect($user->firstName)->toBe($newData['firstName']);
+    expect($user->lastName)->toBe($newData['lastName']);
+    expect($user->city)->toBe($newData['city']);
+});
