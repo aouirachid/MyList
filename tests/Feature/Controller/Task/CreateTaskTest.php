3247e082a0d5c6aa4000feff0df000e559dd636f
rachidaoui <aouirachid22@gmail.com>
2025-08-15 10:43:06 +0100

    Update test to handle file upload and tags

--- tests/Feature/Controller/Task/CreateTaskTest.php
+++ tests/Feature/Controller/Task/CreateTaskTest.php
@@ -3,13 +3,18 @@ namespace Tests\Feature\Controller\Task;
 
 use App\Models\Task;
 use App\Models\User;
+use App\Models\Tag;
+use App\Models\Document;
 use Illuminate\Support\Carbon;
 use Illuminate\Testing\Fluent\AssertableJson;
 use Tymon\JWTAuth\Contracts\Providers\JWT;
 use Tymon\JWTAuth\Facades\JWTAuth;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Storage;
 
 test('An authenticated user successfully create a top-level task',function(){
     $user = User::factory()->create();
+    $tag = Tag::factory()->create();
     $token = JWTAuth::fromUser($user);
     $task = [
         'user_id' => $user->id,
@@ -20,6 +25,7 @@ test('An authenticated user successfully create a top-level task',function(){
         'endDate' => Carbon::now()->addDay()->format('Y-m-d H:i:s'),
         'priority' => 1,
         'parentTaskId' => null,
+        'tag_id' => $tag->id,
         'status' => 1,
     ];
 
@@ -38,6 +44,7 @@ test('An authenticated user successfully create a top-level task',function(){
             ->where('data.description', $task['description'])
             ->where('data.user_id', $user->id)
             ->where('data.parentTaskId', null)
+            ->where('data.tag_id', $tag->id)
             ->etc()
     );
     $this->assertDatabaseHas('tasks', $task);
@@ -45,6 +52,7 @@ test('An authenticated user successfully create a top-level task',function(){
 
 test('An authenticated user successfully create a sub task', function () {
     $user = User::factory()->create();
+    $tag = Tag::factory()->create();
     $token = JWTAuth::fromUser($user);
     $firstTask = [
         'user_id' => $user->id,
@@ -55,6 +63,7 @@ test('An authenticated user successfully create a sub task', function () {
         'endDate' => Carbon::now()->addDay()->format('Y-m-d H:i:s'),
         'priority' => 1,
         'parentTaskId' => null,
+        'tag_id' => $tag->id,
         'status' => 1,
     ];
 
@@ -77,6 +86,7 @@ test('An authenticated user successfully create a sub task', function () {
         'endDate' => Carbon::now()->addDay()->format('Y-m-d H:i:s'),
         'priority' => 1,
         'parentTaskId' => $firstTaskId,
+        'tag_id' => $tag->id,
         'status' => 1,
     ];
 
@@ -96,13 +106,55 @@ test('An authenticated user successfully create a sub task', function () {
             ->where('data.description', $secondTask['description'])
             ->where('data.user_id', $user->id)
             ->where('data.parentTaskId', $firstTaskId)
+            ->where('data.tag_id', $tag->id)
             ->etc()
     );
     $this->assertDatabaseHas('tasks', $secondTask);
 });
 
+test('An authenticated user successfully create a task with document upload', function () {
+    $user = User::factory()->create();
+    $tag = Tag::factory()->create();
+    $document = Document::factory()->create();
+    $token = JWTAuth::fromUser($user);
+
+    $task = [
+        'user_id' => $user->id,
+        'document_id' => $document->id,
+        'title' => 'Test Task with Document',
+        'description' => 'Test Description',
+        'startDate' => Carbon::now()->format('Y-m-d H:i:s'),
+        'endDate' => Carbon::now()->addDay()->format('Y-m-d H:i:s'),
+        'priority' => 1,
+        'parentTaskId' => null,
+        'tag_id' => $tag->id,
+        'status' => 1,
+    ];
+
+    $response = $this->postJson(
+        '/api/v1/tasks',
+        $task,
+        [
+            'Authorization' => 'Bearer ' . $token
+        ]
+    );
+
+    $response->assertStatus(201);
+    $response->assertJson(
+        fn(AssertableJson $json) =>
+        $json->where('message', 'Task created successfully')
+            ->where('data.title', $task['title'])
+            ->where('data.document_id', $document->id)
+            ->where('data.tag_id', $tag->id)
+            ->etc()
+    );
+
+    $this->assertDatabaseHas('tasks', $task);
+});
+
 it('handle validation errors when creating a task', function () {
     $user = User::factory()->create();
+    $tag = Tag::factory()->create();
     $token = JWTAuth::fromUser($user);
 
     $task = [
@@ -113,6 +165,7 @@ it('handle validation errors when creating a task', function () {
         'startDate' => Carbon::now()->format('Y-m-d H:i:s'),
         'priority' => 1,
         'parentTaskId' => null,
+        'tag_id' => $tag->id,
         'status' => 1,
     ];
 
@@ -120,12 +173,13 @@ it('handle validation errors when creating a task', function () {
         'Authorization' => 'Bearer ' . $token
     ]);
     $response->assertStatus(422);
-    $response->assertJsonValidationErrors(['message']);
-    $response->assertDatabaseMissing('tasks', $task);
+    $response->assertJsonValidationErrors(['endDate']);
+    $this->assertDatabaseMissing('tasks', $task);
 });
 
 it("can't create a task with a parent task that doesn't exist", function () {
     $user = User::factory()->create();
+    $tag = Tag::factory()->create();
     $token = JWTAuth::fromUser($user);
 
     $childTask = [
@@ -136,13 +190,66 @@ it("can't create a task with a parent task that doesn't exist", function () {
         'startDate' => Carbon::now()->format('Y-m-d H:i:s'),
         'endDate' => Carbon::now()->addDay()->format('Y-m-d H:i:s'),
         'priority' => 1,
-        'parentTaskId' => 3,
+        'parentTaskId' => 999,
+        'tag_id' => $tag->id,
         'status' => 1,
     ];
 
     $childTaskResponse = $this->postJson('/api/v1/tasks', $childTask, [
         'Authorization' => 'Bearer ' . $token
     ]);
-    $childTaskResponse->assertStatus(403);
+    $childTaskResponse->assertStatus(422);
+    $childTaskResponse->assertJsonValidationErrors(['parentTaskId']);
+    $this->assertDatabaseCount('tasks', 0);
+});
+
+it("can't create a task with a tag that doesn't exist", function () {
+    $user = User::factory()->create();
+    $token = JWTAuth::fromUser($user);
+
+    $task = [
+        'user_id' => $user->id,
+        'document_id' => null,
+        'title' => 'Test Task',
+        'description' => 'Test Description',
+        'startDate' => Carbon::now()->format('Y-m-d H:i:s'),
+        'endDate' => Carbon::now()->addDay()->format('Y-m-d H:i:s'),
+        'priority' => 1,
+        'parentTaskId' => null,
+        'tag_id' => 999,
+        'status' => 1,
+    ];
+
+    $response = $this->postJson('/api/v1/tasks', $task, [
+        'Authorization' => 'Bearer ' . $token
+    ]);
+    $response->assertStatus(422);
+    $response->assertJsonValidationErrors(['tag_id']);
+    $this->assertDatabaseCount('tasks', 0);
+});
+
+it("can't create a task with a document that doesn't exist", function () {
+    $user = User::factory()->create();
+    $tag = Tag::factory()->create();
+    $token = JWTAuth::fromUser($user);
+
+    $task = [
+        'user_id' => $user->id,
+        'document_id' => 999,
+        'title' => 'Test Task',
+        'description' => 'Test Description',
+        'startDate' => Carbon::now()->format('Y-m-d H:i:s'),
+        'endDate' => Carbon::now()->addDay()->format('Y-m-d H:i:s'),
+        'priority' => 1,
+        'parentTaskId' => null,
+        'tag_id' => $tag->id,
+        'status' => 1,
+    ];
+
+    $response = $this->postJson('/api/v1/tasks', $task, [
+        'Authorization' => 'Bearer ' . $token
+    ]);
+    $response->assertStatus(422);
+    $response->assertJsonValidationErrors(['document_id']);
     $this->assertDatabaseCount('tasks', 0);
 });
